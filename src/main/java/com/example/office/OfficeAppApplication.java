150bd1e8d903d2dcace9b85a43040dc0e07bd0cf
ReddyNaresh <madarasureddynaresh@gmail.com>
2022-07-07 09:53:51 +0530

    graga

---
+++ src/main/java/com/example/office/OfficeAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.office;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficeAppApplication.class, args);
+	}
+
+}
